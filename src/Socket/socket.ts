d01a5e1f3615e2cbe12ec53872c3188608d1a680
nstar-y <gimnastiarbahri12@sma.belajar.id>
2024-04-29 01:31:34 +0700

    update

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -129,7 +129,7 @@ export const makeSocket = (config: SocketConfig) => {
 	/** send a binary node */
 	const sendNode = (frame: BinaryNode) => {
 		if(logger.level === 'trace') {
-			logger.trace(binaryNodeToString(frame), 'xml send')
+			logger.trace({ xml: binaryNodeToString(frame), msg: 'xml send' })
 		}
 
 		const buff = encodeBinaryNode(frame)
@@ -306,7 +306,7 @@ export const makeSocket = (config: SocketConfig) => {
 		}
 	}
 
-	const onMessageRecieved = (data: Buffer) => {
+	const onMessageReceived = (data: Buffer) => {
 		noise.decodeFrame(data, frame => {
 			// reset ping timeout
 			lastDateRecv = new Date()
@@ -319,7 +319,7 @@ export const makeSocket = (config: SocketConfig) => {
 				const msgId = frame.attrs.id
 
 				if(logger.level === 'trace') {
-					logger.trace(binaryNodeToString(frame), 'recv xml')
+					logger.trace({ xml: binaryNodeToString(frame), msg: 'recv xml' })
 				}
 
 				/* Check if this is a response to a message we sent */
@@ -546,8 +546,27 @@ export const makeSocket = (config: SocketConfig) => {
 		const ciphered = aesEncryptCTR(authState.creds.pairingEphemeralKeyPair.public, key, randomIv)
 		return Buffer.concat([salt, randomIv, ciphered])
 	}
+	
+	const sendWAMBuffer = (wamBuffer: Buffer) => {
+		return query({
+			tag: 'iq',
+			attrs: {
+				to: S_WHATSAPP_NET,
+				id: generateMessageTag(),
+				xmlns: 'w:stats'
+			},
+			content: [
+				{
+					tag: 'add',
+					attrs: {},
+					content: wamBuffer
+				}
+			]
+		})
+	}
 
-	ws.on('message', onMessageRecieved)
+	ws.on('message', onMessageReceived)
+	
 	ws.on('open', async() => {
 		try {
 			await validateConnection()
@@ -728,6 +747,7 @@ export const makeSocket = (config: SocketConfig) => {
 		requestPairingCode,
 		/** Waits for the connection to WA to reach a state */
 		waitForConnectionUpdate: bindWaitForConnectionUpdate(ev),
+		sendWAMBuffer,
 	}
 }
 
